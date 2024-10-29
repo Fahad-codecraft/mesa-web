e3d3f42c05c02680eff36be8a6c36100f04e957d
Fahad Devnikar <devnikarfahad@gmail.com>
2024-10-29 15:19:00 +0530

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from 'next/font/google'
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const poppins = Poppins({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
@@ -27,6 +28,7 @@ export default function RootLayout({
         <Navbar />
         {children}
       </body>
+      <GoogleAnalytics gaId="G-KZWCF5PD86" />
     </html>
   );
 }
